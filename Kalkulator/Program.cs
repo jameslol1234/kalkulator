643700fe55f79e3f676acaaca534fcce2a0d0170
jameslol1234 <55647640+jameslol1234@users.noreply.github.com>
2019-09-22 09:42:10 +0200

    Add div to menu (#2)

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -18,7 +18,11 @@ namespace Kalkulator
                 Console.WriteLine("1. Dodawanie");
                 Console.WriteLine("2. XAXAXA");
                 Console.WriteLine("3. Mnozenie");
+<<<<<<< HEAD
                 Console.WriteLine("4. Wyjscie");
+=======
+                Console.WriteLine("4. Dzielenie");
+>>>>>>> 833f5f0... Add div to menu
                 int menuOption = Convert.ToInt32(Console.ReadLine());
                 if (menuOption == 4)
                     break;
