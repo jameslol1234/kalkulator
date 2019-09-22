1273142ae0a6dc371fb88c83b01eafd310e8e17c
jameslol1234 <krecik20001@wp.pl>
2019-09-22 09:27:33 +0200

    superchange

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -12,12 +12,15 @@ namespace Kalkulator
         {
             while(true)
             {
-                Console.WriteLine("0. Wyjscie");
+                int aa = 2;
+                if (aa == 4)
+                    break;
                 Console.WriteLine("1. Dodawanie");
-                Console.WriteLine("2. Odejmowanie");
+                Console.WriteLine("2. XAXAXA");
                 Console.WriteLine("3. Mnozenie");
+                Console.WriteLine("4. Wyjscie");
                 int menuOption = Convert.ToInt32(Console.ReadLine());
-                if (menuOption == 0)
+                if (menuOption == 4)
                     break;
 
             }
