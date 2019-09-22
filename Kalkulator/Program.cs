c3a31a4c1eb9423cc791293a88b60863c4ea457c
jameslol1234 <krecik20001@wp.pl>
2019-09-22 09:02:00 +0200

    add menu to Main function

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -10,6 +10,16 @@ namespace Kalkulator
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                Console.WriteLine("0. Wyjscie");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 0)
+                    break;
+
+            }
         }
     }
 }
